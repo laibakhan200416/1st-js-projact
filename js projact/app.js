b803ec3a9860a56db428e56eb80c9f332789d1ca
laiba khan <laibakhan200416@gmail.com>
2024-10-06 14:24:13 +0500

    Add files via upload

---
+++ js projact/app.js
@@ -0,0 +1,21 @@
+// 1st queatin
+// alert("wellcome to  javascrpt❤️");
+
+// 2nd queation
+// alert("hope you doing well enjoy a new language of js " );
+
+// 3rd queation
+// alert("welcom to JS land...\n Happy coading");
+
+// 4th queation
+// alert("welcom to js");
+// alert("Happy coading!!.\n ❤️js is a programming language");
+
+// 5th queation
+// console.log("hey i am laiba khan and i love coading");
+
+// 6th queation
+// cheak html and css file to see answer
+
+// 7th queation
+// for answer see the html file thanks ❤️
